2f808ae9d62a29be4261adadfe7980d2420ae367
Droder4 <roddstkd@gmail.com>
2026-03-17 12:49:36 -0300

    Update Color.kt

--- app/src/main/java/com/roddstkd/admin/ui/theme/Color.kt
+++ app/src/main/java/com/roddstkd/admin/ui/theme/Color.kt
@@ -1,1 +1,11 @@
+package com.roddstkd.admin.ui.theme
 
+import androidx.compose.ui.graphics.Color
+
+val Blue80 = Color(0xFFB3C5FF)
+val BlueGrey80 = Color(0xFFC3C7D6)
+val Teal80 = Color(0xFF9ED9D8)
+
+val Blue40 = Color(0xFF3157C8)
+val BlueGrey40 = Color(0xFF5F657A)
+val Teal40 = Color(0xFF0B8C88)
