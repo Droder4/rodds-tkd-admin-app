730f4753399cff5a7e5ed3aa780c6a07aa94ca12
Droder4 <roddstkd@gmail.com>
2026-03-17 11:12:43 -0300

    Update Theme.kt

--- app/src/main/java/com/roddstkd/admin/ui/theme/Theme.kt
+++ app/src/main/java/com/roddstkd/admin/ui/theme/Theme.kt
@@ -1,1 +1,33 @@
+package com.roddstkd.admin.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColors = darkColorScheme(
+    primary = Blue80,
+    secondary = BlueGrey80,
+    tertiary = Teal80
+)
+
+private val LightColors = lightColorScheme(
+    primary = Blue40,
+    secondary = BlueGrey40,
+    tertiary = Teal40
+)
+
+@Composable
+fun RoddsTKDAdminTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) DarkColors else LightColors
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
