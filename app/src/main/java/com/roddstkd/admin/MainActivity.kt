9eb514b59e3075aebcc477205de6dbc5b94be3f4
Droder4 <roddstkd@gmail.com>
2026-03-17 11:07:04 -0300

    Update MainActivity.kt

--- app/src/main/java/com/roddstkd/admin/MainActivity.kt
+++ app/src/main/java/com/roddstkd/admin/MainActivity.kt
@@ -1,1 +1,18 @@
+package com.roddstkd.admin
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.roddstkd.admin.ui.AdminApp
+import com.roddstkd.admin.ui.theme.RoddsTKDAdminTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoddsTKDAdminTheme {
+                AdminApp()
+            }
+        }
+    }
+}
